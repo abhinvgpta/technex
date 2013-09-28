ec5a600633ac3e0c2ca54ffeeb6d2dec3682570c
Abhinav Gupta <abhinavgupta440@gmail.com>
2013-09-28 16:54:05 +0530

    fifth commit

--- technex/wsgi.py
+++ technex/wsgi.py
@@ -14,20 +14,20 @@ framework.
 
 """
 import os
-from dj_static import Cling
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technex.settings")
+#from dj_static import Cling
+#from django.core.wsgi import get_wsgi_application#
+##os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technex.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 #from django.core.wsgi import get_wsgi_application
 
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
