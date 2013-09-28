5aa21d30170c51ed3bebcee7e75b83d8347a6eab
Abhinav Gupta <abhinavgupta440@gmail.com>
2013-09-28 16:51:08 +0530

    fifth commit

--- technex/wsgi.py
+++ technex/wsgi.py
@@ -14,7 +14,7 @@ framework.
 
 """
 import os
-from dj-static import Cling
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technex.settings")
 
